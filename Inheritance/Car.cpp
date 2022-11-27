fb56c7f4c4ce78f7001659e22279f41c59682f3b
Elizabeth Atkins <elizabeth.atkins@sinclair.edu>
2022-11-26 22:47:16 -0500

    File used for implementing the car class

---
+++ Inheritance/Car.cpp
@@ -0,0 +1,17 @@
+#include "Vehicle.h";
+#include "Car.h";
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
