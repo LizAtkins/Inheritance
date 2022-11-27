bbeede519ca337a170563189875006fb2f1283a0
Elizabeth Atkins <elizabeth.atkins@sinclair.edu>
2022-11-26 22:40:42 -0500

    Add project files.

---
+++ Inheritance/Vehicle.cpp
@@ -0,0 +1,16 @@
+#include "Vehicle.h";
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
---
+++ Inheritance/Vehicle.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef VEHICLE_H 
+#define VEHICLE_H
+
+class Vehicle {
+private:
+	string manufacturer;
+	int year;
+
+public:
+	Vehicle();
+	string getManufacturer() {
+		return manufacturer;
+	}
+	void setManufacturer(string m)  {
+		manufacturer = m;
+	}
+	int getYear() {
+		return year;
+	}
+	void setYear(int y) {
+		year = y;
+	}
+};
+
+
+
+
+
+
+#endif
